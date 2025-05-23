714e1391d05f88f4882cb80034368186086712f5
Кирилл Казаков <kirill.kazakov26@gmail.com>
2025-05-23 10:30:05 -0700

    Initial Commit

---
+++ TodoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by Кирилл Казаков on 23.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
